2b0fc44ad087f6b0577ffbc49b89433a60df9488
vijaykumarsm-44 <mrvijaykumarsm@gmail.com>
2026-08-05 00:02:33 +0530

    Update Risk_Operations.sql

--- Risk_Operations.sql
+++ Risk_Operations.sql
@@ -1,1 +1,15 @@
+/*==============================================================
+                    RISK OPERATIONS ANALYTICS PROJECT
+================================================================
 
+Project      : Risk Operations Analytics
+Database     : RiskOperationsDB
+Tools        : SQL Server, SSMS, Power BI, Excel
+Author       : Vijay
+
+Description:
+This project demonstrates SQL analysis on operational data
+covering Premium, Claims, Risk, Compliance, SLA and
+Business Performance.
+
+==============================================================*/
